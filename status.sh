9baa843b2dd6f71776d1c22278790e9cfbea444e
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-04-10 11:59:02 +0200

    more quiet status

--- status.sh
+++ status.sh
@@ -8,19 +8,19 @@ TOTAL=`$LS $WORKDIR/ffprobeFile/ | wc -l`
 echo "Total files to ingest=$TOTAL"
 
 
-ERRORS=`$LS $WORKDIR/publishedPids/*.error | wc -l`
+ERRORS=`$LS $WORKDIR/publishedPids/*.error 2> /dev/null | wc -l`
 echo "Total files failed=$ERRORS"
 
-SUCCES=`$LS $WORKDIR/publishedPids/* | grep -v error| wc -l`
+SUCCES=`$LS $WORKDIR/publishedPids/* 2> /dev/null | grep -v error| wc -l`
 echo "Total succes =$SUCCES"
 
-START=`$LS $WORKDIR | grep ffprobeFile | sed -e 's/ \+/ /g' | cut -d' ' -f6,7`
+START=`$LS $WORKDIR 2> /dev/null | grep ffprobeFile | sed -e 's/ \+/ /g' | cut -d' ' -f6,7`
 echo "Ingest started at $START"
 
-NEWEST=`$LS $WORKDIR/publishedPids |sed -e 's/ \+/ /g' | cut -d' ' -f6,7 | sort| tail -1`
+NEWEST=`$LS $WORKDIR/publishedPids 2> /dev/null |sed -e 's/ \+/ /g' | cut -d' ' -f6,7 | sort| tail -1`
 echo "newest file through $NEWEST"
 
-rm $WORKDIR/failedProbes.txt
+rm $WORKDIR/failedProbes.txt 2> /dev/null 
 for error in $WORKDIR/publishedPids/*.error; do \
     ERR=`basename $error | cut -d'.' -f1`
    cat $WORKDIR/ffprobeFile/$ERR >> $WORKDIR/failedProbes.txt
