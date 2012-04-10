dfbd212494259833316840d27b96f491927d69a7
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-04-10 15:03:38 +0200

    new version of status

--- status.sh
+++ status.sh
@@ -4,15 +4,23 @@ WORKDIR=$1
 
 LS="ls -l --time-style=long-iso"
 
-TOTAL=`$LS $WORKDIR/ffprobeFile/ 2> /dev/null| wc -l`
-echo "Total files to ingest=$TOTAL"
+function errorsAtStage {
+	STAGE=$1
+	ERRORS=`$LS $WORKDIR/$STAGE_*/*.error 2> /dev/null | wc -l`
+	echo $ERRORS
+}
+
+function succesesAtStage {
+	STAGE=$1
+	SUCCES=`$LS $WORKDIR/$STAGE_*/* 2> /dev/null | grep -v error| wc -l`
+	echo $SUCCES
+}
 
 
-ERRORS=`$LS $WORKDIR/publishedPids/*.error 2> /dev/null | wc -l`
-echo "Total files failed=$ERRORS"
-
-SUCCES=`$LS $WORKDIR/publishedPids/* 2> /dev/null | grep -v error| wc -l`
-echo "Total succes =$SUCCES"
+for i in {1..6}; do \
+	echo "Total succes to at stage $i=`succesesAtStage $i`"
+	echo "Total errors to at stage $i=`errorsAtStage $i`"
+done
 
 START=`$LS $WORKDIR 2> /dev/null | grep ffprobeFile | sed -e 's/ \+/ /g' | cut -d' ' -f6,7`
 echo "Ingest started at $START"
@@ -20,10 +28,10 @@ echo "Ingest started at $START"
 NEWEST=`$LS $WORKDIR/publishedPids 2> /dev/null |sed -e 's/ \+/ /g' | cut -d' ' -f6,7 | sort| tail -1`
 echo "newest file through $NEWEST"
 
-rm $WORKDIR/failedProbes.txt 2> /dev/null 
-touch $WORKDIR/failedProbes.txt 2> /dev/null 
-for error in $WORKDIR/publishedPids/*.error; do \
-    ERR=`basename $error | cut -d'.' -f1`
-   cat $WORKDIR/ffprobeFile/$ERR >> $WORKDIR/failedProbes.txt
-   echo "" >> $WORKDIR/failedProbes.txt
-done
+#rm $WORKDIR/failedProbes.txt 2> /dev/null 
+#touch $WORKDIR/failedProbes.txt 2> /dev/null 
+#for error in $WORKDIR/publishedPids/*.error; do \
+#    ERR=`basename $error | cut -d'.' -f1`
+#   cat $WORKDIR/ffprobeFile/$ERR >> $WORKDIR/failedProbes.txt
+#   echo "" >> $WORKDIR/failedProbes.txt
+#done
