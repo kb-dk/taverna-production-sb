2a2791fbe4fb887e6615caa9c1f2276b7c46e931
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-04-11 11:32:50 +0200

    fixed bug in workflow. optimised status.sh

--- status.sh
+++ status.sh
@@ -6,13 +6,13 @@ LS="ls -l --time-style=long-iso"
 
 function errorsAtStage {
 	local STAGE=$1
-	local ERRORS=`$LS $WORKDIR/${STAGE}_*/*.error 2> /dev/null | wc -l`
+	local ERRORS=`find $WORKDIR/${STAGE}_*/ -name *.error -type f 2> /dev/null | wc -l`
 	_RET=$ERRORS
 }
 
 function succesesAtStage {
 	local STAGE=$1
-	local SUCCES=`$LS $WORKDIR/${STAGE}_*/* 2> /dev/null | grep -v error| wc -l`
+	local SUCCES=`find $WORKDIR/${STAGE}_*/ -type f 2> /dev/null | grep -v error| wc -l`
 	_RET=$SUCCES
 }
 
@@ -24,7 +24,7 @@ for i in {10..99}; do \
 		continue
 	fi
 	file=`basename $dir`
-	echo "Stage $i"
+	echo "Stage $file"
 	succesesAtStage $i
 	echo "Total succes $_RET"
 	errorsAtStage $i
@@ -33,16 +33,3 @@ for i in {10..99}; do \
 	echo
 done
 
-START=`$LS $WORKDIR 2> /dev/null | grep ffprobeFile | sed -e 's/ \+/ /g' | cut -d' ' -f6,7`
-echo "Ingest started at $START"
-
-NEWEST=`$LS $WORKDIR/10_* 2> /dev/null |sed -e 's/ \+/ /g' | cut -d' ' -f6,7 | sort| tail -1`
-echo "newest file through $NEWEST"
-
-#rm $WORKDIR/failedProbes.txt 2> /dev/null 
-#touch $WORKDIR/failedProbes.txt 2> /dev/null 
-#for error in $WORKDIR/publishedPids/*.error; do \
-#    ERR=`basename $error | cut -d'.' -f1`
-#   cat $WORKDIR/ffprobeFile/$ERR >> $WORKDIR/failedProbes.txt
-#   echo "" >> $WORKDIR/failedProbes.txt
-#done
